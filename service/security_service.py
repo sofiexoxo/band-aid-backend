3e3b7a7cc59b45b228c03070174e0debad113aef
Sofie Cai <sofie.cai@ucll.be>
2024-04-17 10:08:55 +0200

    logout, bands test

--- service/security_service.py
+++ service/security_service.py
@@ -12,7 +12,7 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 def hash_password(password: str):
     return pwd_context.hash(password)
 
-def verify_password(plain_password: str, hashed_password: str):
+def verify_password(plain_password: str, hashed_password: str) :
     return pwd_context.verify(plain_password, hashed_password)
 
 def create_access_token(data: dict, expires_delta: timedelta):
--- test_root.py
+++ test_root.py
@@ -1,13 +1,22 @@
-
 from fastapi.testclient import TestClient
 from main import app
 
 client = TestClient(app)
 
-
-
-
+# Test if the root endpoint ("/") returns a successful message
 def test_root_endpoint():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"message": " successful"}
+
+# Test if the logout endpoint ("/logout") returns a successful message
+def test_logout_endpoint():
+    response = client.post("/logout")
+    assert response.status_code == 200
+    assert response.json() == {"message": "Logout successful"}
+
+# Test if the bands endpoint ("/bands") returns a list of bands
+def test_get_bands_endpoint():
+    response = client.get("/bands")
+    assert response.status_code == 200
+    assert len(response.json()) > 0
