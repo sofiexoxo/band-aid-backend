e7da25bbca1d71caf6a5fb83581efd15ae81a0e7
Sofie Cai <sofie.cai@ucll.be>
2024-04-26 11:57:45 +0200

    update database

--- database/database_connection.py
+++ database/database_connection.py
@@ -1,13 +1,13 @@
 import asyncpg
-from asyncpg import Connection  # Add this import statement
-from database.database_config import DATABASE_URL
+from asyncpg import Connection
+from database.database_config import DATABASE_URL  
 
 class Database:
-    def __init__(self, dsn: str):
+    def __init__(self, dsn: str = DATABASE_URL): 
         self.dsn = dsn
 
     async def connect(self) -> Connection:
-        connection = await asyncpg.connect(self.dsn)  # Use self.dsn instead of DATABASE_URL
+        connection = await asyncpg.connect(self.dsn)  
         try:
             return connection
         finally:
@@ -23,3 +23,9 @@ class Database:
     async def fetch(self, query: str, *args):
         async with self.connect() as connection:
             return await connection.fetch(query, *args)
+
+    async def get_user(self, email): 
+        query = "SELECT * FROM users WHERE email = $1"
+        async with self.connect() as connection:
+            record = await connection.fetchrow(query, email)
+            return record
